0a070e8c45bd7207a5b1767d5baadb013a80a4e2
fsorzzoni <ferminsorzzoni@gmail.com>
2026-04-13 11:34:22 -0300

    app.js basico y auth module

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import passport from "passport";
+import "./config/passport.js";
+import authRoutes from "./modules/auth/auth.routes.js";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(passport.initialize());
+
+app.use("/api/auth", authRoutes);
+
+export default app;
