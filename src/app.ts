1c511746d68476c6a22227fc7ebac4701cd79f27
fsorzzoni <ferminsorzzoni@gmail.com>
2026-06-08 11:22:44 -0300

    update cors

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-  origin: ['https://unitracker-frontend.onrender.com'],
+  origin: ['https://unitracker.dev'],
   credentials: true
 }))
 app.use(cookieParser());
